3be30db7b5bc8284166d3eea4cea4bd3e82efbc2
semight <davidoluwasemiloorei@gmail.com>
2024-04-03 09:05:34 +0100

    done with member page

---
+++ src/components/platform/MemberCard.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import React, { FC } from 'react';
+import Avatar from '@/assets/Ellipse_593.png';
+import { BiCalendar } from 'react-icons/bi';
+
+interface iMemberCardProp {
+    name: string;
+    date: string;
+    message: string;
+    email: string;
+}
+
+const MemberCard: FC<iMemberCardProp> = ({ name, date, message, email }) => {
+    return (
+        <>
+        <div>
+            <div className="flex justify-between items-center">
+                <div className="flex items-center gap-3">
+                    <Image src={Avatar} alt={''} width={50} height={50} />
+                    <div>
+                        <h2 className='text-black font-cocogoose text-lg'>{name}</h2>
+                        <p className='text-base text-gray-10 font-semibold'>{email}</p>
+                    </div>
+                </div>
+
+                <div className="flex items-center gap-1 text-gray-10 text-base font-cocogoose">
+                    <span className='flex items-center gap-1'> <BiCalendar /> Joined</span>
+                    <span>{date}</span>
+                </div>
+            </div>
+            <p className='text-black text-lg font-semibold mt-4'>{message}</p>
+            </div>
+        </>
+    )
+}
+
+export default MemberCard;
--- src/components/platform/Members.tsx
+++ src/components/platform/Members.tsx
@@ -1,19 +1,51 @@
 import React from 'react';
 import GroupCard from './GroupCard';
-import { IoIosArrowRoundBack } from "react-icons/io";
+import MemberCard from './MemberCard';
 
 
 const Members = () => {
+
+    const member = [
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+        { name: 'Adedimeji Ajayi', email: '@dimeji', date: 'Feb 23, 2024', message: 'This class will give you all the insights for great and successful user reseah you will learn the basics of UX research and come up.' },
+      ];
+
     return (
         <>
             <div className="flex gap-8">
                 <div className="w-[70%]">
-                    <div
-                        className="flex items-center gap-2 text-light-blue font-cocogoose-light font-bold text-[18px] cursor-pointer"
-                        onClick={() => { }}
-                    >
-                        <IoIosArrowRoundBack size={"30px"} />
-                        <p>Back</p>
+                    <div className="flex mb-10 gap-12 items-center">
+                        <button className='bg-brand rounded-2xl px-6 h-8 font-cocogoose text-white'>All</button>
+                        <button className='border-none font-cocogoose-light text-lg text-black flex items-center justify-center gap-2'>Members <span className='font-cocogoose text-lg'>200</span></button>
+                        <button className='border-none font-cocogoose-light text-lg text-black flex items-center justify-center gap-2'>Admins <span className='font-cocogoose text-lg'>38</span></button>
+                        <button className='border-none font-cocogoose-light text-lg text-black flex items-center justify-center gap-2'>Online <span className='font-cocogoose text-lg'>100</span></button>
+                    </div>
+
+                    <div className='flex flex-col gap-8'>
+                        {member.map((member, i) => {
+                            return (
+                                <MemberCard
+                                    key={i}
+                                    name={member.name}
+                                    date={member.date}
+                                    email={member.email}
+                                    message={member.message}
+                                />
+                            );
+                         })}
                     </div>
 
                 </div>
