88c018765966ccc697780ade1d14ce013924e5e3
semight <davidoluwasemiloorei@gmail.com>
2024-04-04 04:48:47 +0100

    code modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import NavBar from '@/components/resuable/NavBar/NavBar'
+// import NavBar from '@/components/resuable/NavBar/NavBar'
 
-export default function Home() {
-  return (
-    <>
-    <NavBar index={-1} />
-    <h1>Home Page</h1>
-    </>
-  );
+// export default function Home() {
+//   return (
+//     <>
+//     <NavBar index={-1} />
+//     <h1>Home Page</h1>
+//     </>
+//   );
+// }
+
+
+import AboutUsPage from "@/components/about/About";
+
+
+export const metadata = {
+  title: "About Us",
+};
+
+export default function About() {
+  return <AboutUsPage />
 }
