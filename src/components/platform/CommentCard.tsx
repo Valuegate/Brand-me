c88abbe5d1ea5c12530eff69900e181d0d2b4c52
semight <davidoluwasemiloorei@gmail.com>
2024-04-06 11:39:26 +0100

    done with the mobile view

--- src/components/platform/CommentCard.tsx
+++ src/components/platform/CommentCard.tsx
@@ -23,7 +23,7 @@ const CommentCard: FC<iCommentCardProp> = ({ name, date, message }) => {
                     </div>
                     <p className='text-lg font-cocogoose'>Hide</p>
                 </div>
-                <div className="px-8 py-8">
+                <div className="px-8 md:px-2 py-8">
                     <div className="flex items-center gap-3">
                         <Image src={Avatar} alt={''} width={50} height={50} />
                         <div>
@@ -31,9 +31,9 @@ const CommentCard: FC<iCommentCardProp> = ({ name, date, message }) => {
                         <p className='text-base text-gray-10 font-semibold'>{date}</p>
                         </div>
                     </div>
-                    <p className='text-black text-base font-cocogoose mt-4'>{message}</p>
+                    <p className='text-black text-base md:text-[15px] font-cocogoose mt-4'>{message}</p>
 
-                    <div className="flex items-center mt-5 justify-between">
+                    <div className="flex items-center mt-5 md:flex-col justify-between">
                         <div className="flex items-center gap-4">
                             <div className="flex gap-1">
                                 <ThumbUpIcon className='text-light-blue'/>
@@ -43,7 +43,7 @@ const CommentCard: FC<iCommentCardProp> = ({ name, date, message }) => {
                                 <CommentIcon className='text-gray-10' />
                                 <h4 className='text-gray-10 text-base'>28</h4>
                             </div>
-                            <div className='flex items-center'>
+                            <div className='flex items-center md:flex-col'>
                                 <div className="flex items-center">
                                     <Image src={Avatar} alt={''} width={20} height={20} />
                                     <Image src={Avatar} alt={''} width={20} height={20} />
@@ -53,7 +53,7 @@ const CommentCard: FC<iCommentCardProp> = ({ name, date, message }) => {
                                 <h4 className='text-gray-10 text-base'>New comments 2 days ago</h4>
                             </div>
                         </div>
-                        <Link href={''}>
+                        <Link href={''} className='md:mt-4'>
                         <h2 className='text-sm font-cocogoose text-light-blue'>View comments</h2>
                         </Link>
                     </div>
--- src/components/resuable/QuizNavBar.tsx
+++ src/components/resuable/QuizNavBar.tsx
@@ -1,23 +1,20 @@
 "use client";
 
 import React from "react";
-
 import { IoIosArrowRoundBack } from "react-icons/io";
-
 import { useQuizStore } from "@/stores/quizStore";
+import Link from 'next/link';
 
 const QuizNavBar = () => {
   const score = useQuizStore((state) => state.score);
 
   return (
     <div className="w-full h-[10vh] bg-brand flex items-center justify-between justify-items-center px-12 md:px-[5%] border-none rounded-b-[40px] md:rounded-b-[25px]">
-      <div
-        className="flex items-center gap-2 text-white font-cocogoose-light font-bold text-[18px] cursor-pointer"
-        onClick={() => {}}
-      >
+      <Link
+        className="flex items-center gap-2 text-white font-cocogoose-light font-bold text-[18px] cursor-pointer" href={'/platform'}      >
         <IoIosArrowRoundBack size={"26px"} />
         <p>Back</p>
-      </div>
+      </Link>
 
       <h2 className="font-cocogoose text-white text-[20px]">50:00:00</h2>
     </div>
